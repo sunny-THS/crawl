9c1d70fcc24a6f38441b3123f119305e3e5ed3b0
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-11-14 22:59:20 +0700

    Add files via upload

--- scrapes.js
+++ scrapes.js
@@ -1,7 +1,10 @@
 const puppeteer = require('puppeteer');
 
 async function scrapeChannel(url) {
-  const browser = await puppeteer.launch({args: [--no-sandbox]});
+  const browser = await puppeteer.launch({args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+  ]});
   const page = await browser.newPage();
   await page.goto(url);
 
